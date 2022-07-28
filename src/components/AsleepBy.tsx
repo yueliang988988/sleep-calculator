8f357169ca8cfb94f6220ab0b6d432cf328fb17b
jessef <jesse.fraust@gmail.com>
2022-07-28 02:00:52 -0700

    install react-select; grab state of selected hours;

--- src/components/AsleepBy.tsx
+++ src/components/AsleepBy.tsx
@@ -1,15 +1,42 @@
 import React, {useState, useEffect} from 'react';
 import useCalculateCycle, {SleepCycleQuery, SleepCycleResult} from "../utils/useCalculateCycle";
-import date from 'date-and-time';
+// import date from 'date-and-time';
+import Select, {SingleValue} from 'react-select';
 
-
+// type OptionType = {
+//   value: string, 
+//   label: string
+// }
 const AsleepBy: React.FC = () => {
-
+  const [selectHours, setSelectHours] = useState<SingleValue<{value: string, 
+    label: string}>>()
   // const [currTime, setCurrTime] = useState<Date | null>();
   const { timeArray, message } = useCalculateCycle({
     time: "10:30 AM",
     method: "asleepBy"
   });
+
+  const options = [
+    { value: "1", label: "1"},
+    { value: "2", label: "2"},
+    { value: "3", label: "3"},
+    { value: "4", label: "4"},
+    { value: "5", label: "5"},
+    { value: "6", label: "6"},
+    { value: "7", label: "7"},
+    { value: "8", label: "8"},
+    { value: "9", label: "9"},
+    { value: "10", label: "10"},
+    { value: "11", label: "11"},
+    { value: "12", label: "12"}
+  ];
+
+  const handleChangeHours = (obj: SingleValue<{value: string, 
+    label: string}>) => {
+    setSelectHours(obj);
+  }
+
+  console.log("selected: ", selectHours?.value)
   
 
 
@@ -31,9 +58,17 @@ const AsleepBy: React.FC = () => {
         /> */}
 
         {/* ! LETS DO SINGLE DROP DOWN INPUTS */}
+        <Select 
+          options={options}
+          instanceId={"hours"}
+          // className="w-20"
+          onChange={(option) => handleChangeHours(option)}
+          blurInputOnSelect
+        />
 
         <p>
           {/* {currTime === "" ? "--:-- --" : currTime} */}
+          {selectHours?.value ? selectHours?.value : "pick something"}
         </p>
 
         <p>
