531a4a7f62234a205f9e4eaa29e04e63008148f4
jessefd <1319957+jfrausto@users.noreply.github.com>
2022-09-21 19:59:07 -0700

    svg links

--- src/server/env-schema.js
+++ src/server/env-schema.js
@@ -2,7 +2,7 @@ const { z } = require("zod");
 
 const envSchema = z.object({
   // Specify your environment variables schema here
-  X_RAPID_API_KEY: z.string().min(1)
+  // X_RAPID_API_KEY: z.string().min(1)
 });
 
 module.exports.envSchema = envSchema;
