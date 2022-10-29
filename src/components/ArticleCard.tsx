9137eb245358cbc4176df10fd1b48221cbad38fb
jessef <jesse.fraust@gmail.com>
2022-10-28 22:53:06 -0700

    make article source clickable; adjust padding

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -13,7 +13,7 @@ const ArticleCard: React.FC<ArticleCardProps> = ({title, description, datePublis
   let patternNew = date.compile('MMM DD, YYYY');
   return (
     <div
-      className='flex flex-col justify-start items-start gap-1 px-4 py-3 w-full
+      className='flex flex-col justify-start items-start gap-3 px-5 py-4 w-full
               bg-slate-300 rounded-md'
     >
       <h3
@@ -30,7 +30,7 @@ const ArticleCard: React.FC<ArticleCardProps> = ({title, description, datePublis
         {/* 
           slice the date we need off the data, and transform the date into desired format/pattern
         */}
-        {date.transform(datePublished.slice(0, 10), patternOld, patternNew)} --- <u>{url}</u>
+        {date.transform(datePublished.slice(0, 10), patternOld, patternNew)} --- <a href={url} target="_blank" rel='noopener noreferrer'><u>{url}</u></a>
       </p>
     </div>
   )
